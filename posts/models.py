72590362a9d1d509261a2f28402ed57b74e03384
mywave76 <mywave.reloaded@yahoo.com>
2017-05-06 17:04:52 +0200

    Updated Admin

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,10 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-	title = models.CharField(max_length=120)
-	content = models.TextField()
-	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-	timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-	def __unicode__(self):
-		return self.title
-		
+    def __unicode__(self):
+        return self.title
