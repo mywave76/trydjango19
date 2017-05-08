5e6977e16ec4d9295e9545e40b02450951e2ea7e
mywave76 <mywave.reloaded@yahoo.com>
2017-05-08 11:33:08 +0200

    Update views.py detail view, add html page

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from .models import Post
@@ -8,10 +8,13 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
+    #instance = Post.objects.get(id=10)
+    instance = get_object_or_404(Post, id=2)
     context = {
-        "title": "Detail"
+        "title": instance.title,
+        "instance": instance
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
 	# if request.user.is_authenticated():
