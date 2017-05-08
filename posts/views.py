19242b9b12abb0735a76dc7fea76acc4c8605731
mywave76 <mywave.reloaded@yahoo.com>
2017-05-08 14:40:06 +0200

    Add edit capability to post/form

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
 from .forms import PostForm
@@ -14,6 +14,7 @@ def post_create(request):
         instance = form.save(commit=False)
         print(form.cleaned_data.get("title"))
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
         "form": form
@@ -49,8 +50,21 @@ def post_list(request):
     }
     return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print(form.cleaned_data.get("title"))
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
