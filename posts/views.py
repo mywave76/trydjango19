45caff9aec42fa60805996f11ec0088c1be49036
mywave76 <mywave.reloaded@yahoo.com>
2017-05-08 11:50:01 +0200

    Add dynamic routing

--- posts/views.py
+++ posts/views.py
@@ -7,9 +7,9 @@ from .models import Post
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
+def post_detail(request, id=None):
     #instance = Post.objects.get(id=10)
-    instance = get_object_or_404(Post, id=2)
+    instance = get_object_or_404(Post, id=id)
     context = {
         "title": instance.title,
         "instance": instance
