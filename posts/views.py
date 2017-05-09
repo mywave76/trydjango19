10d35ae79df4246545815acc5eaab2f1ebd43407
mywave76 <mywave.reloaded@yahoo.com>
2017-05-09 23:30:28 +0200

    Enable slug in models, views and urls

--- posts/views.py
+++ posts/views.py
@@ -26,9 +26,9 @@ def post_create(request):
     }
     return render(request, "post_form.html", context)
 
-def post_detail(request, id=None):
+def post_detail(request, slug=None):
     #instance = Post.objects.get(id=10)
-    instance = get_object_or_404(Post, id=id)
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance
@@ -69,8 +69,8 @@ def post_list(request):
     }
     return render(request, "post_list.html", context)
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -86,8 +86,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted!")
     return redirect("posts:list")
