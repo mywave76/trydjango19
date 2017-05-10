464f37b9ce062c845f57e732c037cf5a0a019ea6
mywave76 <mywave.reloaded@yahoo.com>
2017-05-10 18:05:42 +0200

    Associate users to posts with foreign key

--- posts/migrations/urlify.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-from urllib import quote_plus
-
-register = template.Library()
-
-@register.filter
-def urlify(value):
-	return quote_plus(value)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, redirect, render
 
 from urllib import quote_plus
@@ -13,12 +13,19 @@ def post_create(request):
     # if request.method == "POST":
     #     print(request.POST.get("title"))
     #     print(request.POST.get("content"))
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
+    #if not request.user.is_authenticated:
+    #    raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print(form.cleaned_data.get("title"))
+        instance.user = request.user
         instance.save()
         messages.success(request, "Successfully created!")
+        #print(form.cleaned_data.get("title"))
         return HttpResponseRedirect(instance.get_absolute_url())
     else:
         messages.error(request, "Creation failed!")
@@ -74,6 +81,8 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -91,6 +100,8 @@ def post_update(request, slug=None):
     return render(request, "post_form.html", context)
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted!")
